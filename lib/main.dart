55ec46b0577a967cabb6811f71210c81cd21ad18
phantomcorn <jj.phantakorn@outlook.com>
2021-07-10 15:30:20 +0700

    prototypeof main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+
+import "package:latlong2/latlong.dart";
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,33 +14,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Time Constraint'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,68 +31,246 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget main() {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+    return SafeArea(
+        child : Scaffold (
+            body: Center(
+                child: Container(
+                  color: Colors.grey,
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top : height / 6 , bottom : width / 8),
+                        child: Text("Where to go?",
+                            style: TextStyle(
+                                fontSize: width * 0.06,
+                                color: Colors.black
+                            )
+                        ),
+                      ),
+                      Container(
+                          width: width / 1.5,
+                          child: Align(alignment: Alignment.center,
+                              child: Text("Current location",
+                                  style: TextStyle(
+                                      fontSize: width * 0.05,
+                                      color: Colors.black
+                                  )
+                              )
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              border: Border.all(
+                                  color: Colors.blueAccent
+                              )
+                          )
+                      ),
+                      Icon(
+                        Icons.arrow_downward,
+                        size: width * 0.5,
+                      ),
+                      Container(
+                          width: width / 1.5,
+                          child: Align(alignment: Alignment.center,
+                              child: Text("Destination",
+                                  style: TextStyle(
+                                      fontSize: width * 0.05,
+                                      color: Colors.black
+                                  )
+                              )
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              border: Border.all(
+                                  color: Colors.blueAccent
+                              )
+                          )
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            Map();
+                          },
+                          child: Text("GO!",
+                              style: TextStyle(
+                                  fontSize: width * 0.05,
+                                  color: Colors.black
+                              )
+                          )
+                      )
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.start,
+                  ),
+                )
+            )
+        )
+    );
+  }
+
+  /*
+  Widget mode() {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+    return SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: ,
+          )
+        )
+    );
+  }
+   */
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+    return SafeArea(
+        child : Scaffold (
+            body: Center(
+                child: Container(
+                  color: Colors.grey,
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top : height / 6 , bottom : width / 8),
+                        child: Text("Where to go?",
+                            style: TextStyle(
+                                fontSize: width * 0.06,
+                                color: Colors.black
+                            )
+                        ),
+                      ),
+                      Container(
+                          width: width / 1.5,
+                          child: Align(alignment: Alignment.center,
+                              child: Text("Current location",
+                                  style: TextStyle(
+                                      fontSize: width * 0.05,
+                                      color: Colors.black
+                                  )
+                              )
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              border: Border.all(
+                                  color: Colors.blueAccent
+                              )
+                          )
+                      ),
+                      Container(
+                        width: width * 0.5,
+                        height:  height / 3,
+                        child: Stack(
+                          children: [
+                            Container(
+                                alignment: Alignment.center,
+                                child: Icon(
+                                  Icons.arrow_downward,
+                                  size: width * 0.5,
+                                )
+                            ),
+                            Align(
+                              alignment: Alignment.center,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                },
+                                child: Text("Mode"),
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.red
+                                ),
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                          width: width / 1.5,
+                          child: Align(alignment: Alignment.center,
+                              child: Text("Destination",
+                                  style: TextStyle(
+                                      fontSize: width * 0.05,
+                                      color: Colors.black
+                                  )
+                              )
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.yellow,
+                              border: Border.all(
+                                  color: Colors.blueAccent
+                              )
+                          )
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            Map();
+                          },
+                          child: Text("GO!",
+                              style: TextStyle(
+                                  fontSize: width * 0.05,
+                                  color: Colors.black
+                              )
+                          )
+                      )
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.start,
+                  ),
+                )
+            )
+        )
+    );
+  }
+  }
+
+
+
+class Map extends StatefulWidget {
+
+  @override
+  State<Map> createState() => _MapState();
+
+}
+
+class _MapState extends State<Map> {
+
+  late MapController controller;
+
+  @override
+  void initState() {
+    controller = MapController();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return FlutterMap(
+      mapController: controller,
+      options: MapOptions(
+        center: LatLng(51.5, -0.09),
+        zoom: 13.0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+      layers: [
+        TileLayerOptions(
+          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          subdomains: ['a', 'b', 'c']
+        ),
+        MarkerLayerOptions(
+          markers: [
+            Marker(
+              width: 80.0,
+              height: 80.0,
+              point: LatLng(51.5, -0.09),
+              builder: (ctx) =>
+                Container(
+                  child: FlutterLogo(),
+                ),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        )
+      ]
     );
   }
+
 }
