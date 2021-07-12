81baee1603b486429503ff7f1866cfa2be5ff5f1
phantomcorn <jj.phantakorn@outlook.com>
2021-07-12 12:22:58 +0700

    add API key for google map use in ios app + add RequestAssistant class for http request + add google map package

---
+++ lib/RequestAssistant.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class RequestAssistant {
+
+  static Future<dynamic> getRequest(String url) async {
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    try {
+
+      if (response.statusCode == 200) {
+        String jsonData = response.body;
+        var decodeDate = jsonDecode(jsonData);
+        return decodeDate;
+
+      } else {
+
+        return "Failed, No Response.";
+
+      }
+
+    } catch (exp) {
+
+      return "Failed.";
+
+    }
+  }
+
+}
